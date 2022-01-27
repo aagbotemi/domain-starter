5f5c1ab505db5ff22f27669e580b1ec187507f18
Abiodun Awoyemi <aabiodunawoyemi@gmail.com>
2022-01-27 11:27:10 +0100

    beneficiaries

---
+++ routes/beneficiaries.js
@@ -0,0 +1,56 @@
+var express = require("express");
+var router = express.Router();
+const { beneficiariesController } = require("../controllers/beneficiaries");
+const { jwtAuth } = require("../middleware/auth");
+const { upload } = require("../middleware/upload");
+
+router.get("/:id", jwtAuth.generalVerifyToken, beneficiariesController.getById);
+
+router.get(
+  "/",
+  jwtAuth.adminVerifyToken,
+  beneficiariesController.getAllBeneficiaries
+);
+
+router.get(
+  "/po/trainees",
+  jwtAuth.poVerifyToken,
+  beneficiariesController.getPOTrainees
+);
+
+router.get(
+  "/:state",
+  jwtAuth.adminVerifyToken,
+  beneficiariesController.getTraineesinState
+);
+
+router.get("/po/:state", jwtAuth.poVerifyToken, beneficiariesController.getPo);
+
+router.get(
+  "/:grad",
+  jwtAuth.adminVerifyToken,
+  beneficiariesController.getTraineesByGraduationStatus
+);
+
+router.get(
+  "/po/:grad",
+  jwtAuth.poVerifyToken,
+  beneficiariesController.getPOTraineesbyGraduationStatus
+);
+
+router.post(
+  "/",
+  jwtAuth.poVerifyToken,
+//   upload.fields([{ profileImage: 'avatar' }, { curriculumVitae: 'gallery' }]),
+  beneficiariesController.createTrainee
+);
+
+router.put(
+  "/:id",
+  jwtAuth.poVerifyToken,
+  beneficiariesController.updateTrainee
+);
+
+router.delete("/:id", jwtAuth.poVerifyToken, beneficiariesController.delete);
+
+module.exports = router;
